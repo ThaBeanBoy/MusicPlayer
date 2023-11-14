94346aeeee115de6cef11fcecf1b1dd3a0c95110
ThaBeanBoy <gtchipoyera@gmail.com>
2023-11-14 15:19:48 +0200

    Dialog working perfect and imported the Button component from a previous project.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,29 @@ import { songType } from './types';
 
 import * as Slider from '@radix-ui/react-slider';
 import * as Progress from '@radix-ui/react-progress';
+import * as Dialog from '@radix-ui/react-dialog';
 
-import { BsFillPlayFill, BsPauseFill, BsSearch } from 'react-icons/bs';
-import { AiFillCaretLeft } from 'react-icons/ai';
+import { AiFillCaretLeft, AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import {
+  BsFillPlayFill,
+  BsPauseFill,
+  BsSearch,
+  BsSkipEnd,
+  BsSkipStart,
+} from 'react-icons/bs';
+import { BiCommentDetail } from 'react-icons/bi';
+import { FiShare2 } from 'react-icons/fi';
+import { PiCaretDownBold } from 'react-icons/pi';
+import { MdPlaylistAdd } from 'react-icons/md';
 
 import { millisToMinutesAndSeconds } from './hooks/Time';
-import { useInput } from './input';
 import { useSongTime } from './hooks/Time';
 
+import { useInput } from './components/input';
+import Button from './components/button';
+
 import './audioControls.css';
+import { cn } from './utils/cn';
 
 const songs: songType[] = [
   {
@@ -130,7 +144,9 @@ function App() {
           <AiFillCaretLeft />
         </button>
         <p className='font-bold'>Birthday Playlist</p>{' '}
-        <button className='ml-auto'>share</button>
+        <button className='ml-auto'>
+          <FiShare2 />
+        </button>
       </div>
       <img
         className='w-full rounded-3xl mt-4'
@@ -147,7 +163,7 @@ function App() {
         <BsSearch className='absolute right-2 bottom-3 translate-y-[-2px]' />
       </div>
 
-      <ul className='pb-[105px]'>
+      <ul className={cn({ 'pb-[74.8px]': currentSong })}>
         {songs
           .filter(({ title, artists, features }) => {
             if (searchInput.valueIsEmpty) return true;
@@ -177,9 +193,7 @@ function App() {
       {currentSong && (
         <div
           id='audio-controls-container'
-          className='group fixed bottom-0 w-full bg-white z-50 left-1/2 translate-x-[-50%] border-t text-lg px-2 pb-2'
-          data-aria-expanded={audioPlayerExpanded}
-          data-expanded={audioPlayerExpanded}
+          className='group fixed bottom-0 w-full bg-white z-40 left-1/2 translate-x-[-50%] border-t text-lg px-2 pb-2'
         >
           <Progress.Root className='relative overflow-hidden bg-blue-200 rounded-full w-full h-[2px] mb-2'>
             <Progress.Indicator
@@ -193,51 +207,122 @@ function App() {
           </Progress.Root>
 
           <div id='audio-controls' className='mx-auto max-w-2xl'>
-            <div className='group-data-[expanded=false]:hidden w-full mb-3 flex gap-3 items-center text-sm'>
-              <span>{songTime.progress.get.string()}</span>
-              <Slider.Root
-                className='relative flex items-center select-none flex-1 touch-none w-[200px] h-5'
-                max={100}
-                value={[songTime.progress.get.percentage()]}
-                onValueChange={(newTimePercentnage) => {
-                  if (audioPlayer.current) {
-                    songTime.progress.set.percentage(newTimePercentnage[0]);
-                    audioPlayer.current.currentTime =
-                      songTime.progress.get.seconds();
-                  }
-                }}
-              >
-                <Slider.Track className='bg-blue-200 relative grow rounded-full h-[3px]'>
-                  <Slider.Range className='absolute bg-blue-500 rounded-full h-full' />
-                </Slider.Track>
-                <Slider.Thumb className='block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-black rounded-[10px] hover:bg-blue-100 focus:outline-none focus:shadow-[0_0_0_5px] focus:border-blue-500' />
-              </Slider.Root>
-
-              <span>
-                {millisToMinutesAndSeconds(currentSong.song.duration)}
-              </span>
-            </div>
-
             {/* song info */}
             <div className='flex gap-2 w-full items-center'>
-              <button className='flex flex-1 gap-2 justify-start'>
-                <img
-                  src={currentSong.song.coverUrl}
-                  width={56}
-                  height={56}
-                  className='rounded-lg border'
-                  alt='song cover'
-                />
+              <Dialog.Root>
+                <Dialog.Trigger asChild>
+                  <button
+                    className='flex flex-1 gap-2 justify-start'
+                    onClick={() => setaudioPlayerExpanded(true)}
+                  >
+                    <img
+                      src={currentSong.song.coverUrl}
+                      width={56}
+                      height={56}
+                      className='rounded-lg border'
+                      alt='song cover'
+                    />
 
-                <div>
-                  <span className='font-bold'>{currentSong.song.title}</span>
-                  <br />
-                  <span className='text-gray-600 text-sm'>
-                    {displayArtists(currentSong.song.artists)}
-                  </span>
-                </div>
-              </button>
+                    <div>
+                      <span className='font-bold'>
+                        {currentSong.song.title}
+                      </span>
+                      <br />
+                      <span className='text-gray-600 text-sm'>
+                        {displayArtists(currentSong.song.artists)}
+                      </span>
+                    </div>
+                  </button>
+                </Dialog.Trigger>
 
+                <Dialog.Portal>
+                  <Dialog.Content className='w-screen h-screen fixed bg-white z-50 left-0 top-0 flex justify-center'>
+                    <div id='song-section' className='max-w-xl px-4'>
+                      <Dialog.Close className='text-2xl ml-auto mb-4' asChild>
+                        <Button icon={<PiCaretDownBold />} variant='flat' />
+                      </Dialog.Close>
+                      <img
+                        src={currentSong.song.coverUrl}
+                        className='rounded-3xl border mx-auto'
+                        alt='song cover'
+                        width={420}
+                      />
+
+                      <Button
+                        icon={<AiOutlineHeart />}
+                        label='8.2k'
+                        variant='hollow'
+                        className='flex-row-reverse shadow-none px-2 py-1 border-blue-300'
+                      />
+
+                      <Button
+                        icon={<BiCommentDetail />}
+                        label='720'
+                        variant='hollow'
+                        className='flex-row-reverse px-2 py-1 shadow-none border-blue-300'
+                      />
+
+                      <Button
+                        icon={<MdPlaylistAdd />}
+                        label='Add to playlist'
+                        variant='hollow'
+                        className='flex-row-reverse px-2 py-1 shadow-none border-blue-300'
+                      />
+
+                      <Button
+                        icon={<FiShare2 />}
+                        label='Share'
+                        variant='hollow'
+                        className='flex-row-reverse px-2 py-1 shadow-none border-blue-300'
+                      />
+
+                      <div className='flex items-center gap-8 mx-auto'>
+                        <Button
+                          icon={<BsSkipStart />}
+                          className='text-[32px]'
+                          variant='flat'
+                        />
+                        <button
+                          onClick={handlePlayPause}
+                          className='text-[64px]'
+                        >
+                          {audioPlaying ? <BsPauseFill /> : <BsFillPlayFill />}
+                        </button>
+                        <button className='text-[32px]'>
+                          <BsSkipEnd />
+                        </button>
+                      </div>
+
+                      <div className='w-full mb-3 flex gap-3 items-center text-sm'>
+                        <span>{songTime.progress.get.string()}</span>
+                        <Slider.Root
+                          className='relative flex items-center select-none flex-1 touch-none w-[200px] h-5'
+                          max={100}
+                          value={[songTime.progress.get.percentage()]}
+                          onValueChange={(newTimePercentnage) => {
+                            if (audioPlayer.current) {
+                              songTime.progress.set.percentage(
+                                newTimePercentnage[0]
+                              );
+                              audioPlayer.current.currentTime =
+                                songTime.progress.get.seconds();
+                            }
+                          }}
+                        >
+                          <Slider.Track className='bg-blue-200 relative grow rounded-full h-[3px]'>
+                            <Slider.Range className='absolute bg-blue-500 rounded-full h-full' />
+                          </Slider.Track>
+                          <Slider.Thumb className='block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-black rounded-[10px] hover:bg-blue-100 focus:outline-none focus:shadow-[0_0_0_5px] focus:border-blue-500' />
+                        </Slider.Root>
+
+                        <span>
+                          {millisToMinutesAndSeconds(currentSong.song.duration)}
+                        </span>
+                      </div>
+                    </div>
+                  </Dialog.Content>
+                </Dialog.Portal>
+              </Dialog.Root>
               <button
                 onClick={handlePlayPause}
                 className='text-[48px] border-l'
---
+++ src/components/button.tsx
@@ -0,0 +1,57 @@
+import { cn } from '../utils/cn';
+import { forwardRef } from 'react';
+export type ButtonProps = {
+  label?: React.ReactNode;
+  icon?: React.ReactElement;
+  variant?: 'fill' | 'hollow' | 'flat';
+  desctructive?: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      label,
+      icon,
+      variant = 'fill',
+      desctructive = false,
+      className,
+      disabled,
+      ...props
+    },
+    ref
+  ) => (
+    <button
+      className={cn(
+        'bg-blue-500 border-blue-500 flex items-center justify-center gap-2 rounded-3xl border px-4 py-2 font-semibold capitalize text-white shadow-md',
+        {
+          // hollow variant
+          'text-blue-500 bg-gray-100 ': variant === 'hollow',
+
+          // icon only
+          'h-10 w-10 rounded-2xl p-0': !label,
+
+          //desctructive
+          'border-red-500 bg-red-500': desctructive,
+
+          // flat
+          'text-blue-500 border-none bg-white shadow-none': variant === 'flat',
+
+          'text-red-500':
+            (desctructive && variant === 'hollow') ||
+            (variant === 'flat' && desctructive),
+
+          'brightness-75': disabled,
+        },
+        className
+      )}
+      ref={ref}
+      {...props}
+    >
+      {label} {icon}
+    </button>
+  )
+);
+
+Button.displayName = 'Button';
+
+export default Button;
